9271be58a8ac4483aed7fffcd6c773c2a1ff6248
alisha-web <au630097@gmail.com>
2024-07-30 22:27:54 +0500

    Update .js

--- .js
+++ .js
@@ -1,1 +1,65 @@
-.
+//Chapter 1 (Alerts)
+//1- Alert these following (individually):
+// I. First Name
+// II. Last Name
+// III. Email
+// IV. Phone Number
+// V. Password
+alert("First Name");
+alert("Last Name");
+alert("Email");
+alert("Phone Number");
+alert("Password");
+
+//2. Correct this statement: alert"You're learning JavaScript!";
+alert("You're learning JavaScript!");
+
+//3. Code an alert statement displaying any message you like.
+alert("Welcome to JavaScript programming!");
+
+//Chapter 2 (Variables for string)
+//1. Declare any variable in the camelCase format.
+var myVariable;
+
+//2. Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice.
+var myString;
+myString = "Hello World";
+ 
+//3. Declare the variable teamName and Alert your Team name.
+var teamName = "Dream Team";
+alert(teamName);
+
+//4. This statement has already been coded. var bestMan = "Charlie"; Assign the variable a new string.
+var bestMan = "Charlie";
+bestMan = "John";
+
+//Chapter 3 (Variables for numbers)
+//1. Declare a variable “caseQty”
+var caseQty;
+
+//2. Assign to the variable caseQty, which has already been declared, the value 144.
+caseQty = 144;
+
+//3. Rewrite this statement so the variable can be used in a math operation. var num = "9";
+var num = 9;
+
+//4. In one statement declare a variable. In a second statement assign it the sum of 2 numbers.
+var sum;
+sum = 5 + 7;
+
+// 5. What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// Try it yourself.
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+// orderTotal is 110
+
+// 6. In the first statement declare a variable and assign it a number. In
+// the second statement, change the value of the variable by adding
+// it together with a number.
+var number = 20;
+number = number + 5;
+
